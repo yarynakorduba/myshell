61946105b62f05c09326fdc30f9a70f03d03401d
YuriyVilnitskiy <laba@ucu.edu.ua>
2017-05-05 20:48:55 +0300

    initial commit

--- main.cpp
+++ main.cpp
@@ -4,158 +4,151 @@
 #include <limits.h>
 #include <dirent.h>
 #include <sstream>
+#include <cstring>
+#include <vector>
+#include <boost/bind.hpp>
+#include <sys/stat.h>
+#include <fstream>
+#include <errno.h>
 
 using namespace std;
 
 #define PATH_MAX 4096
 
-char *curr_dir = (char *) "/usr/bin";
-/*
-string comm;
-string argum;
+int mpwd(char** argv){
+    if(argv[1] == NULL){
+        cout << get_current_dir_name();
+    }else if((string(argv[1])=="-h")||(string(argv[1])=="--help")){
+        cout<<"Display current directory"<<endl;
+    }
 
-int splitting(string l) {
-    string arr[3];
+    return 0;
+}
 
-    int i = 0;
-    stringstream ssin(l);
-
-    while (ssin.good() && i < 3) {
-        ssin >> arr[i];
-
-        ++i;
-        if(i == 3){
-            printf("ERROR : TU DAUN! TAK NE MOZNA PYSATY.");
-            return -1;
+int mcd(char** argv){
+    if ( argv[1] == NULL){
+        chdir ("/home/yuriy");
+    }else if((string(argv[1])=="-h")||(string(argv[1])=="--help")){
+        cout<<"command mcd change directory \n"
+                "just mcd - will move you to your home directory \n"
+                "mcd and ../ - will move you to previous directory \n"
+                "mcd and directory to which you want to go - will move you to the desired directory"<<endl;
+    }
+    else{
+        DIR *dir = opendir((const char *) argv[1]);
+        if (dir != NULL){
+            chdir(argv[1]);}
+        else{
+            cout<<"No such file or directory";
         }
-
     }
-    comm = arr[0];
-
-    argum = arr[1];
-    cout << comm << endl;
-    cout << argum;
-    return 0;
-
-
-}*/
-string command;
-string args;
-void split( string line){
-    int i = 0;
-    while(line[i] != ' '){
-        i++;
-    }command = line.substr(0,i);
-    args = line.substr(i+1, sizeof(line));
-
-}
-
-int ls(){
-/*
-    char cwd[1024];
-    chdir("/path/to/change/directory/to");
-    getcwd(cwd, sizeof(cwd));
-    printf( "%s\n", cwd); }
-    */
-    DIR *dp = NULL;
-    struct dirent *dptr = NULL;
-    unsigned int count = 0;
-
-    dp = opendir((const char*)curr_dir);
-    if(NULL == dp)
-    {
-        printf("\n ERROR : Could not open the working directory\n");
-        return -1;
-    }
-
-    for(count = 0; NULL != (dptr = readdir(dp)); count++)
-    {
-        // Check if the name of the file/folder begins with '.'
-        // If yes, then do not display it.
-        if(dptr->d_name[0] != '.')
-            printf("%s  ",dptr->d_name);
-    }
-
     return 0;
 }
+void mexit(char** argv){
+    if((string(argv[1])=="-h")||(string(argv[1])=="--help")){
+        cout<<"mexit will finish your program"<<endl;
+
+    } else{
+        int castInputToInt;
+        stringstream ss(argv[1]);
+        ss >> castInputToInt;
+        exit(castInputToInt);
+    }
+}
+
+
 
 int main(int argc, char* argv[], char**env)
 {
-/*
-    pid_t pid = fork();
+    chdir("/usr/bin");
 
-    if (pid == -1)
-    {
-        cout << "ERROR" << &endl;
-    }
-    else if (pid > 0)
-    {
-        int status;
-        waitpid(pid, &status, 0);
-    }
-    else {
-        char cwd[1024];
-        getcwd(cwd, sizeof(cwd));
-        //printf( "%s\n", cwd);
-        execve("/bin/ls", argv, env);
-        _exit(EXIT_FAILURE);
-    }*/
-    string input = "";
-    char *ptr;
-    int a = 10;
+    while(true)  {
 
-    while( a < 20 )  {
-        a++;
-        //string command;
-        // char commandChar[1024];    // Will store the command entered by user in character array
-        // char *argVector[10];    // Will store the arguments after they are tokenized
-        //int argCount;        // Will store the number of arguments
+        int i = 0;
+        cout << get_current_dir_name() << "$ ";
+        char command[128];
+        cin.getline( command, 128 );
 
-        //char cwd[1024];
-        //getcwd(cwd, sizeof(cwd));
-        //printf( "%s$\n", cwd);
 
-        cout << curr_dir << "$ ";
-        getline(cin, input);
-        //cout << input;
-        split(input);
-        cout<<command<<endl;
-        cout<<args;
-        //cout << *arg;
-        if (input == "pwd"){
-            cout << curr_dir;
+        vector<char*> args;
+        char* prog = strtok( command, " " );
+        char* tmp = prog;
+
+        if(tmp == NULL){
+            continue;
         }
-        else if ( input == "ls"){
-            ls();
 
+        while ( tmp != NULL )
+        {
+            if (tmp[0] == '#')
+                break;
+            args.push_back( tmp );
+            i++;
+            tmp = strtok( NULL, " " );
         }
-        else if(input == "cd"){
 
+        char** argv = args.data();
+
+
+        argv[args.size()] = NULL;
+
+        if ((string(prog)=="mpwd")){
+            mpwd(argv);
+        }
+        else if((string(prog)=="mcd")) {
+            mcd(argv);
+        }
+        else if((string(prog)=="mexit")) {
+            mexit(argv);
+        }
+        else if((string(prog)=="mls")||(string(prog)=="mrv")||(string(prog)=="mrm")||
+                (string(prog)=="mmkdir")||(string(prog)=="mcpp")){
+            pid_t kidpid = fork();
+
+            if (kidpid < 0)
+            {
+                perror( "Internal error: cannot fork." );
+                return -1;
+            }
+            else if (kidpid == 0)
+            {
+                // I am the child.
+
+                //const char* a = prog;
+                //const char* b = "/home/yuriy/CLionProjects/myRV/";
+                //const char* b = "";
+                //char* p=new char[strlen(b)+strlen(a)+1];
+                //char* c=new char[strlen(a)+1];
+                //strcat(p,b);
+                //strcat(p,a);
+                //strcpy(c,p);
+                //printf("Name %s\n", a);
+                //printf("Path %s\n", getenv("PATH"));
+
+                //puts(prog);
+                int err = execvp (prog, argv);
+                if(err == -1)
+                {
+                    printf("Err: %i : %s\n", errno, strerror(errno));
+                }
+                // The following lines should not happen (normally).
+                //perror( command );
+                return -1;
+            }
+            else
+            {
+                // I am the parent.  Wait for the child.
+                if ( waitpid( kidpid, 0, 0 ) < 0 )
+                {
+                    perror( "Internal error: cannot wait for child." );
+                    return -1;
+                }
+            }
+        } else {
+            cout<<argv[0]<<" :command not found"<<endl;
+            //continue;
         }
 
         cout << "\n";
-
-
-        //cout << input << endl;
-
-
-
-        /*
-        ptr = strtok(commandChar, " ");
-        argVector[argCount] = ptr;
-        argCount++;
-        while (ptr != NULL)
-        {
-            ptr = strtok(commandChar, " ");
-            cout << ptr << " " << argCount << "\n";
-            argVector[argCount] = ptr;
-            argCount++;
-            //execvp(argvec[0], argvec);
-            //fatal("exec failed");
-        }
-*/
     }
-
-    return 0;
-
 }
