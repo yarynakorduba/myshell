6c92315348ea7bc3d9c0cae43983bab0ddfcc905
YuriyVilnitskiy <laba@ucu.edu.ua>
2017-05-09 17:33:47 +0300

    Update sh_main.cpp

--- sh_main.cpp
+++ sh_main.cpp
@@ -14,14 +14,16 @@ using namespace std;
 
 int mpwd(char** argv){
     if(argv[1] == NULL){
-        cout << my_get_current_dir_name();
+        cout << get_current_dir_name();
     }else if((string(argv[1])=="-h")||(string(argv[1])=="--help")){
         cout<<"Display current directory"<<endl;
-    }else		
-	{
-		cerr << "Bad pwd option. Usage: [Тут лінь писати -- самі працюйте.]" << endl;
-		return 1; // Error
-	}
+    }
+    else
+    {
+        cerr << "Bad pwd option." << endl;
+        return 1; // Error
+    }
+
 
     return 0;
 }
@@ -97,26 +99,67 @@ void globalHelp(){
             "\n"
             "mcp\n"
             "\n"
+
         <<endl;
 
+}
+int execution(char* prog, char** argv){
+    pid_t kidpid = fork();
+
+    if (kidpid < 0)
+    {
+        perror( "Internal error: cannot fork." );
+        return -1;
+    }
+    else if (kidpid == 0)
+    {
+        // I am the child.
+
+        //const char* b = "/home/yuriy/CLionProjects/myRV/";
+
+        //puts(prog);
+        int err = execvp (prog, argv);
+        if(err == -1)
+        {
+            printf("Err: %i : %s\n", errno, strerror(errno));
+        }
+        // The following lines should not happen (normally).
+        //perror( command );
+        return -1;
+    }
+    else
+    {
+        // I am the parent.  Wait for the child.
+        if ( waitpid( kidpid, 0, 0 ) < 0 )
+        {
+            perror( "Internal error: cannot wait for child." );
+            return -1;
+        }
+    }
+
+
+}
+inline bool FileExistSameDir (const std::string& name) {
+    return ( access( name.c_str(), F_OK ) != -1 );
 }
 
 
 
 int main(int argc, char* argv[], char**env)
 {
-    //! chdir("/usr/bin");
+    chdir("/usr/bin");
 
     while(true)  {
 
         int i = 0;
-        cout << my_get_current_dir_name() << "$ ";
-		const size_t buf_size = 4096;
-        char command[buf_size]; //! УВАГА -- суть змін зрозуміла? Ну і -- не жаднічайте.
+        cout << get_current_dir_name() << "$ ";
+        const size_t buf_size = 4096;
+        char command[buf_size];
         cin.getline( command, buf_size );
 
+
         vector<char*> args;
-        char* prog = strtok( command, " " ); //! strtok краще уникати... Вона має глобальний стан.
+        char* prog = strtok( command, " " );
         char* tmp = prog;
 
         if(tmp == NULL){
@@ -132,59 +175,85 @@ int main(int argc, char* argv[], char**env)
             tmp = strtok( NULL, " " );
         }
 
+        args.push_back( NULL );
+        char** argv = args.data();
+        string progname(prog);
 
-		args.push_back( NULL ); //! В C++ краще використовувати nullptr.
-        //argv[args.size()] = NULL; //! УВАГА Невизначена поведінка! Тамнемає такого елемента!
-		
-        char** argv = args.data(); //! Цей вказівник коректний лише поки args не змінювали. Тому після push_back 
+        if((progname=="mshell")){
+            if(FileExistSameDir(argv[1])){
+                string str1 = argv[1];
+                ifstream file(str1);
+                string str;
+                string delimiter = " ";
+                while (getline(file, str))
+                {
+                    vector<char*> args1;
+                    size_t pos = 0;
+                    string token;
+                    int n = 0;
+                    while ((pos = str.find(" ")) != std::string::npos) {
+                        token = str.substr(0, pos);
+                        char *cstr = new char[token.length() + 1];
+                        strcpy(cstr, token.c_str());
+                        n +=1;
+                        args1.push_back(cstr);
+                        str.erase(0, pos + delimiter.length());
+                    }
 
-		string progname(prog); //! Просто оптимізація
-        if ( progname=="mpwd" ){ 
+                    char *cstr = new char[str.length() + 1];
+                    strcpy(cstr, str.c_str());
+                    n +=1;
+                    args1.push_back(cstr);
+
+                    args1.push_back(NULL);
+                    char** argv1 = args1.data();
+
+                    if ((string(argv1[0])=="mpwd")){
+
+                        mpwd(argv1);
+
+                    }if((string(argv1[0])=="mcd")) {
+
+                        mcd(argv1);
+                    }
+                    else if((string(argv1[0])=="mexit")) {
+
+                        mexit(argv1, n);
+
+                    }
+                    else if((string(argv1[0])=="mhelp")) {
+
+                        globalHelp();
+                    }
+                    else {
+
+                        execution(argv1[0], argv1);
+                    }
+
+                }
+                cout << "\n";
+            }else{
+                cout<<"No such file"<<endl;
+            }
+        }
+        else if ((progname=="mpwd")){
             mpwd(argv);
         }
-        else if( progname=="mcd" ) {
+        else if((progname=="mcd")) {
             mcd(argv);
         }
-        else if((string(prog)=="mexit")) {
+        else if((progname=="mexit")) {
             mexit(argv, i);
+            return -1;
         }
-        else if((string(prog)=="help")) {
+        else if((progname=="help")) {
             globalHelp();
         }
         else{
-            pid_t kidpid = fork();
-
-            if (kidpid < 0)
-            {
-                perror( "Internal error: cannot fork." );
-                return -1;
-            }
-            else if (kidpid == 0)
-            {
-                // I am the child.
-
-                //puts(prog);
-                int err = execvp (prog, argv);
-                if(err == -1)
-                {
-					// Progname printed just for better diagnostic.
-                    printf("Err: %i : %s\n\t Name: %s\n", errno, strerror(errno), prog); 
-                }
-                // The following lines should not happen (normally).
-                //perror( command );
-                return -1;
-            }
-            else
-            {
-                // I am the parent.  Wait for the child.
-                if ( waitpid( kidpid, 0, 0 ) < 0 )
-                {
-                    perror( "Internal error: cannot wait for child." );
-                    return -1;
-                }
-            }
+            execution(prog, argv);
         }
 
         cout << "\n";
+
     }
 }
