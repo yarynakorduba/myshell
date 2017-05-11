e6345066a7528e5f018a3ec54bdda692b26ab06b
yarynakorduba <korduba_y@ucu.edu.ua>
2017-05-11 22:51:06 +0300

    changed the structure of myCP

--- myCP/main.cpp
+++ myCP/main.cpp
@@ -10,19 +10,18 @@
 #include <fstream>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <libgen.h>
 
 using namespace std;
 
 #define PATH_MAX 4096
 
 
-//char *curr_dir = (char *) "/usr/bin";
-
 //! УВАГА! Глобалньі змінні!
-vector<char*> vectorOfPath;
-string command;
-string args;
+
 void splitString( string line){
+    string args;
+    string command;
     int i = 0;
     while(i != sizeof(line)){
         if(line[i] == ' '){
@@ -55,101 +54,127 @@ int rewrite_check() {
     char first = ans[0];
     cout << ans << endl;
     if (first == 'y') {
-        return 0;
+        return 1;
     }
+    if (first == 'n') {
+        return 0;}
+
+    cout << "ERROR IN THE INPUT. THIS FILE WON`T BE COPIED" << endl;
+
     return -1;
 }
 
-int inside_copy(char** argv, int source, int dest) {
-    ifstream f1 (argv[source], fstream::binary);
-    ofstream f2 (argv[dest], fstream::trunc|fstream::binary);
-    f2 << f1.rdbuf();
+
+int inside_copy(char** argv, int source, int dest, int f_trigger) {
+    struct stat st;
+    if (stat(argv[source],&st) != 0) {
+
+        //checking for existance of each file we want to rewrite
+                      cout << "NO SUCH FILE"<<endl;
+                      return -1; }
+    int rewrit = 0;
+    if ((f_trigger == 0) && ((stat(argv[dest],&st) == 0))) {
+        rewrit = rewrite_check();
+    }
+
+    if ((rewrit == 1) || (f_trigger == 1) || (stat(argv[dest],&st) == -1)) {
+        cout << argv[dest] << endl;
+        ifstream f1 (argv[source], fstream::binary);
+        ofstream f2 (argv[dest], fstream::trunc|fstream::binary);
+        f2 << f1.rdbuf();
+
+}
     return 0;
 }
 
-int inside_copy_dir(char** argv, int numb) {
+int inside_copy_dir(char** argv, int numb, int f_trigger) {
     int i=1;
-    std::string additive(argv[1]);
-    if (additive == "-f") {i=2;}
-    while (i != numb) {
-        std::string name(argv[numb]);
-        char str[4096]; //! УВАГА 128 -- мало!
+    struct stat st;
 
-		//! Уникайте strtok!
-		//! Взагалі, який сенс маніпуляцій нижче?
-		//! Ви розрізали по "/", щоб потім по ньому склеїти?! 
-		//! Лише щоб вивести filename?
-		//! УВАГА! Як я наголошував, для такого аналізу є спеціальні функції:
-		//! https://linux.die.net/man/3/dirname
-		//! http://www.boost.org/doc/libs/1_48_0/libs/filesystem/v3/doc/tutorial.html -- tut3.cpp i tut4.cpp
-		//!		is_directory(p), exists(p), .filename() і т.д.
-        strcpy (str, argv[i]);                                                //start of splitting
-        char * token = std::strtok(str, "/");
-        char* temp = token;
-        while (token != NULL) {
-            temp=token;
-            token = strtok(NULL, "/");
+    while (i != numb) {
+        if (string(argv[i]) != "-f") {
+        char *basec, *bname;
+        basec = strdup(argv[i]);
+        bname = basename(basec);
+
+        std::string name1(argv[numb]);
+        name1 += "/" + (string)bname;
+
+        cout << " filename "<< bname << endl;
+
+        const char* nName = name1.c_str();
+
+        if (stat(argv[i],&st) != 0) {
+                    //checking for existance of each file we want to rewrite
+             cout << "NO SUCH FILE bname"<<endl;
         }
-        cout << " filename "<< temp << endl;
-        // const char* spl = "/";
-        name += '/';
-        name+= temp;
-        const char* nName = name.c_str();
-        int ch = 0;
-        if (is_file(nName) && (additive != "-f")) {
-            int check = rewrite_check();
-            if (check == -1) {ch = -1;}
+        else {
+        int rewrit = 0;
+        if ((f_trigger == 0) && ((stat(nName,&st) == 0))) {
+            rewrit = rewrite_check();
         }
-        if ((ch==0) || (additive == "-f") ||!(is_file(temp))) {
-			//! У вас же вище якраз функція для того є!
+
+        if ((rewrit==1) || (f_trigger==1) ||(!(is_file(nName)))) {
             ifstream f1 (argv[i], fstream::binary);                               //start copying file
             ofstream f2 (nName, fstream::trunc|fstream::binary);
             f2 << f1.rdbuf();
-        }
-        i++;
+        }}}
 
+        i++;
     }
-	return 0;
+        return 0;
 }
+
 
 int cp_help(){
     cout << "Usage: cp [-f] SOURCE DEST \n or:  cp [-f] SOURCE... DIRECTORY \n Copy SOURCE to DEST, or multiple SOURCE(s) to DIRECTORY. \n -f - rewrite existing files without asking" << endl;
     return 0;
 }
 
-int main(int a, char** argv) {
-    struct stat st;
-    int i=1;
-    std::string name(argv[1]);
-    if (name=="-h"|| name=="--help") {return cp_help();}
-    if (name == "-f") {i=2;}
-    while (i!=sizeof(argv))   {
-		//! УВАГА! Ви вилазите за масив! у нього немає елемента з номером argv!
-        if (argv[i+1] == NULL) {                                    // pointer is on the last argument
-            if ((i!=1) && (is_dir(argv[i]))) {                     //spec. option for copying few f into dir
-                return inside_copy_dir(argv, i);
-            }
-            else if (((i==1)&&((name!="-h")||(name != "--help"))) ||
-                     ((i>2)&&(name != "-f"))||((i>3)&&(name == "-f"))) {
-                cout << "WRONG ARGUMENT" << endl;
-                return -1;
-            }
-            else if (((((i==2)&&(stat(argv[i],&st) == -1))||
-                       ((i==3)&&(stat(argv[i],&st) == 0)&&(name == "-f"))))) {
-                return inside_copy(argv, i-1, i);
-            }
-            else if((i=2) && (stat(argv[i],&st) == 0)){
-                if (rewrite_check()==0) {
-                    return inside_copy(argv, i-1, i);
-                }
-                else{return -1;}
-            }
-		}
 
-        else {                                             //for all files except the last one
-            if (stat(argv[i],&st) != 0) {                 //checking for existance of each file we want to rewrite
-                cout << "NO SUCH FILE"<<endl;
-                return -1; }}
-			i++; 
-		}
+int main(int num, char** argv) {
+    int help_trigger = 0;
+    int f_trigger = 0;
+    int dir_trigger = 0;
+
+
+    if (is_dir(argv[num-1])) {
+            dir_trigger = 1;
+        }
+
+    int i = 1;
+    while (i<num) {//TRIGGERS TAKE VALUES
+
+        string elm(argv[i]); //! Small optimization
+
+        if (elm == "-f") {
+            f_trigger = 1;
+        }
+
+        else if ((elm == "-h") || (elm == "--help")) {
+            help_trigger = 1;
+            return cp_help();
+        }
+        ++i;
+    }
+
+    if ((num <=2) && (help_trigger != 1)) {
+        cout << "WRONG QUANTITY OF ARGUMENTS";
+        return -1;
+    }
+
+//    if (!is_dir(argv[i-1])
+//        && (f_trigger == 0)) {
+//        cout << "WRONG ARGUMENTS. NO SUCH FILE OR DIRECTORY" << endl;
+//        return -1;
+//    }
+
+    if (dir_trigger == 1) {
+        return inside_copy_dir(argv, i-1, f_trigger);
+    }
+
+    else {
+        return inside_copy(argv, num-2, num-1, f_trigger);
+    }
+
 }
